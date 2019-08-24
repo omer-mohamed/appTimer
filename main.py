ea6b7e3d97988f4a642e4c253b3d0e150ec61423
strats527 <omer.javed26@gmail.com>
2019-08-23 22:35:18 -0400

    Updated an error for hifen counter being null

--- main.py
+++ main.py
@@ -15,46 +15,29 @@ try:
     while True:
         hifen_counter = 0
         hifen_position =[]
-        new_window_name = GetWindowText(GetForegroundWindow())
+        window = GetWindowText(GetForegroundWindow())
 
-        for i, char in enumerate(new_window_name):
+        for i, char in enumerate(window):
             if char == "-":
                 hifen_counter+=1
                 hifen_position.append(i);
 
         if hifen_counter == 1:
-            new_window_name = new_window_name[:hifen_position[0]]
+            active_name = window[:hifen_position[0]]
+
+        elif hifen_counter == 0:
+            active_name = window
         else:
+            print(hifen_counter)
+            print(window)
             size = len(hifen_position)
-            new_window_name = new_window_name[hifen_position[size-2]:hifen_position[size-1]]
-            new_window_name = new_window_name[2:]
-
-        if active_window_name != new_window_name:
-            print(active_window_name)
-            activity_name = active_window_name
-
-            if not first_time:
-                end_time = datetime.datetime.now()
-                time_entry = TimeEntry(start_time, end_time, 0, 0, 0, 0)
-                time_entry._get_specific_times()
-
-                exists = False
-                for activity in activeList.activities:
-                    if activity.name == activity_name:
-                        exists = True
-                        activity.time_entries.append(time_entry)
-
-                if not exists:
-                    activity = Activity(activity_name, [time_entry])
-                    activeList.activities.append(activity)
-                with open('activities.json', 'w') as json_file:
-                    json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
-                    start_time = datetime.datetime.now()
-            first_time = False
-            active_window_name = new_window_name
+            active_name = window[hifen_position[size-2]:hifen_position[size-1]]
+            active_name = active_name[2:]
+        print(active_name)
 
 
-        time.sleep(10)
-except KeyboardInterrupt:
+        time.sleep(6)
+except KeyboardInterrupt:"""
     with open('activities.json', 'w') as json_file:
         json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
+"""
